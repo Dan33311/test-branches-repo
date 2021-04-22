d5e7543c47f914a466689def0b36054dc1bb517f
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-04-22 12:38:44 -0500

    working main view

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const events = [{
+        title: 'Test tile',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test tile 222',
+        createdAt: new Date(),
+        description: 'Test description 222'
+    }]
+    res.render('index', { events: events })
 })
 
 app.use('/events', eventsRouter)
